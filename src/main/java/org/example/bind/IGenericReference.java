cd7c520b16b7352f68571b61d83fe81a91fd4ee1
Niudfear <hello@itcast.cn>
2024-11-04 18:22:20 +0800

    TEST

---
+++ src/main/java/org/example/bind/IGenericReference.java
@@ -0,0 +1,8 @@
+package org.example.bind;
+
+/**
+ * 统一泛化调用接口
+ */
+public interface IGenericReference {
+    String $invoke(String args);
+}
---
+++ src/main/java/org/example/session/SessionChannelInitializer.java
@@ -0,0 +1,31 @@
+package org.example.session;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import org.example.session.handlers.SessionServerHandler;
+
+/**
+ * 会话管道初始化类
+ */
+public class SessionChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final Configuration configuration;
+
+    public SessionChannelInitializer(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+        ChannelPipeline line = channel.pipeline();
+        line.addLast(new HttpRequestDecoder());
+        line.addLast(new HttpResponseEncoder());
+        line.addLast(new HttpObjectAggregator(1024 * 1024));
+        line.addLast(new SessionServerHandler(configuration));
+    }
+
+}
